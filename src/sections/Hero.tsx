17432d3218b44b976cd6d08c0514636c00359fbc
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-09-29 04:19:56 +0500

    Hero section content completed

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,23 +4,27 @@ import memojiImage from "../assets/images/memoji-computer.png"
 import ArrowDown from '../assets/icons/arrow-down.svg'
 
 export const HeroSection = () => {
-  return <div className="py-32">
+  return <div className="py-32 md:py-48 lg:py-60">
    <div className="container">
+    <div className="flex flex-col items-center">
     <Image src={memojiImage} alt="Memoji" className="size-[100px]" />
-    <div>
-      <div></div>
-      <div>Available for new projects</div>
+    <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+      <div className="bg-green-500 size-2.5 rounded-full"></div>
+      <div className="text-sm font-medium">Available for new projects</div>
     </div>
-    <h1>Building Exceptional User Experiences</h1>
-    < p>I specialize in transforming designs into functional, high performance mobile and web applications with the robust and scaleable backends. Let's discuss your next project.</p>
-    <div>
-      <button>
-        <span>Explore My Work</span>
-        <ArrowDown />
+    </div>
+  <div className="max-w-lg mx-auto">
+  <h1 className="font-serif text-3xl text-center mt-8 tracking-wide md:text-5xl md:font-bold">Building Exceptional User Experiences</h1>
+  <p className="mt-4 text-center text-white/60 md:text-lg">I specialize in transforming designs into functional, high performance mobile and web applications with the robust and scaleable backends. Let's discuss your next project.</p>
+  </div>
+    <div className="flex md:flex-row sm:flex-col md:justify-center items-center mt-8 gap-4">
+      <button className="inline-flex items-center gap-2 border border-white/15 px-5 h-12 rounded-xl">
+        <span className="font-semibold ">Explore My Work</span>
+        <ArrowDown className="size-4" />
       </button>
-      <button>
+      <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-lg">
         <span>👋</span>
-        <span>Let's Connect</span>
+        <span className="font-semibold">Let's Connect</span>
         
       </button>
     </div>
