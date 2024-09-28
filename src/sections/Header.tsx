d60dd5b88aff169cba125307895df633f464c772
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-09-28 13:59:29 +0500

    Project work started

---
+++ src/sections/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return <div className="flex justify-center items-center relative top-3">
+    <nav>
+      <a href="#" className="px-4 py-1.5 rounded-full text-white/70 text-sm font-semibold">Home</a>
+      <a href="#" className="px-4 py-1.5 rounded-full text-white/70 text-sm font-semibold">Projects</a>
+      <a href="#" className="px-4 py-1.5 rounded-full text-white/70 text-sm font-semibold">About</a>
+      <a href="#" className="px-4 py-1.5 rounded-full text-white/70 text-sm font-semibold">Contact</a>
+    </nav>
+  </div>;
+};
